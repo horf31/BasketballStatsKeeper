8ff5316ff7e53009e9de5775891aea44c8b0dcbf
Danchen Chen <dan.chen@affirm.com>
2020-04-12 18:00:36 -0700

    Add layouts

---
+++ app/src/main/java/com/dancc/basketballstatskeeper/RecordActivity.java
@@ -0,0 +1,13 @@
+package com.dancc.basketballstatskeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_record);
+  }
+}
